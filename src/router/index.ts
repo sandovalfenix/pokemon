b0d975719c8a1346220113ae65777e820984f6da
Hermis Julian <hermisjulian555@gmail.com>
2025-11-25 21:25:56 -0500

    feat: add Centro Pokémon component and healing store

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/centropokemon',
+      name: 'centropokemon',
+      component: () => import('../components/CentroPokemon.vue'),
+    },
   ],
 })
 
